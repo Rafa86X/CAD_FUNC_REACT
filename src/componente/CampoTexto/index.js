57579399aa518971dab69a7559c688b459a9b047
Rafael Junges <rafadevweb@gmail.com>
2023-06-28 08:16:06 -0300

    setup inicial do projeto

---
+++ src/componente/CampoTexto/index.js
@@ -0,0 +1,14 @@
+import './campotexto.css'
+
+const CampoTexto = () =>{
+
+    return (
+        <div className="campo-texto">
+            <label >Nome:</label>
+            <input tipe="text" />
+        </div>
+    )
+
+}
+
+export default CampoTexto;
