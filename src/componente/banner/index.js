f04f0ded20d5e0fe6c8e23550f1cda1192dedf76
Rafael Junges <rafadevweb@gmail.com>
2023-06-28 08:16:30 -0300

    config inicial do projeto

---
+++ src/componente/banner/index.js
@@ -0,0 +1,11 @@
+import './banner.css'
+
+const Banner = () =>{
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal do organo"/>
+        </header>
+    )
+}
+
+export default Banner
