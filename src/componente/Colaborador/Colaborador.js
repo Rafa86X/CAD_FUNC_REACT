ca32b4bbc94a92bd765e3105547187ab5a2b5c1f
Rafael Junges <rafadevweb@gmail.com>
2023-06-30 12:57:03 -0300

    criando card colaborador

---
+++ src/componente/Colaborador/Colaborador.js
@@ -0,0 +1,20 @@
+import './Colaborador.css'
+
+const Colaborador = () =>{
+    return(   
+
+        <div className='colaborador'>
+            <div  className='cabecalho'>
+                <img src='https://github.com/rafa86x.png' alt="img"/>
+            </div>
+            <div className='rodape'>
+                <h4>Rafael Junges</h4>
+                <h5>Instrutos</h5>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Colaborador
