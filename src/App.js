0b4a7c4bf71a2e8d8163a8fa7f3140e655eb660b
Rafael Junges <rafadevweb@gmail.com>
2023-06-30 14:04:19 -0300

    Mapeando colaboaradores

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
 
   const times = [
     {
+      nome: ''
+  },
+    {
       nome: 'Front-End',
       corPrimaria: '#82CFFA',
       corSecundaria: '#E8F8FF',
@@ -45,15 +48,21 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador)=>{
-    console.log(colaborador)
-  
+    setColaboradores([...colaboradores, colaborador])
+    console.log(colaboradores);
   }
 
   return (
     <div className="App">
       <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador =>aoNovoColaboradorAdicionado(colaborador)}/>
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}
+      
+      {times.map(time => <Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria}
+      colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
      </div>
   );
--- src/componente/Colaborador/Colaborador.js
+++ src/componente/Colaborador/Colaborador.js
@@ -1,16 +1,15 @@
 import './Colaborador.css'
 
-const Colaborador = () =>{
+const Colaborador = ({nome, img, cargo}) =>{
     return(   
 
         <div className='colaborador'>
             <div  className='cabecalho'>
-                <img src='https://github.com/rafa86x.png' alt="img"/>
+                <img src={img} alt={nome}/>
             </div>
             <div className='rodape'>
-                <h4>Rafael Junges</h4>
-                <h5>Instrutos</h5>
-
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
         </div>
 
